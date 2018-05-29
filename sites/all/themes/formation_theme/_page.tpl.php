f194e42bf7339bdda925c69c997066a412b8c5ee
Scoty <mianquihin15@gmail.com>
2018-05-29 11:50:55 +0000

    Premier commit

---
+++ sites/all/themes/formation_theme/_page.tpl.php
@@ -0,0 +1,28 @@
+
+
+<div id="page">
+    <div class="wrapper">
+        <div id="header">
+
+        </div>
+        <div id="wrapper_slider">
+            <div id="slider">
+
+            </div>
+        </div>
+        <div id="main-content">
+
+            <div id="full_content" class=" ">
+            </div>
+        </div>
+        <div id="prefooter">
+
+        </div>
+        <div id="footer" class="">
+
+            <div id="full_footer">
+              
+            </div>
+        </div>
+    </div>
+</div>
